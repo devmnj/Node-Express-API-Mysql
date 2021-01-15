d3de07b5fec3910d36db04a7573e66f0e5d9d2cb
manojap <manojap@outlook.com>
2021-01-15 21:46:54 +0530

    .

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const Sequelize=require('sequelize')
+module.exports=new Sequelize('todo_collections','root','123',{
+    host:'127.0.0.1',
+    port:3306,
+    dialect:'mysql',
+    pool:{
+        max:5,
+        min:0,
+        acquire:30000,
+        idle:10000
+    }
+})
